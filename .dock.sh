4be2c55edcbc9ba80e991f0ee41e4e5489a41f85
Mahela Abeysekera <mahela32@gmail.com>
2020-02-18 21:41:27 +0530

    updated with scripts and functions

--- .dock.sh
+++ .dock.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
+#!/bin/bash# prints the input
+function dock() {
+  echo 'Hello Friend'
+}
 
-# prints the input
-function print_my_input() {
-  echo 'Your input: ' $1
+# converts and saves youtube video to mp3
+function convert_to_mp3() {
+  youtube-dl --extract-audio --audio-format mp3 $1
 }
